4c7cd8e85336bf50772b936e4eec7aa54c786f6b
Kellys Bellanger <kbbellanger@uamv.edu.ni>
2026-09-21 16:21:19 -0600

    feat: Agrego main.app

--- src/main/java/ni/edu/uam/evaluacion/MainApp.java
+++ src/main/java/ni/edu/uam/evaluacion/MainApp.java
@@ -1,4 +1,33 @@
 package ni.edu.uam.evaluacion;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+        @Override
+        public void start(Stage stage) {
+            cambiarVentana(stage, "/sistema/view/menu.fxml", "Menú Principal");
+        }
+
+        public static void cambiarVentana(Stage stage, String fxmlPath, String titulo) {
+            try {
+                FXMLLoader loader = new FXMLLoader(MainApp.class.getResource(fxmlPath));
+                Parent root = loader.load();
+                stage.setTitle(titulo);
+                stage.setScene(new Scene(root));
+                stage.centerOnScreen();
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+    }
 }
