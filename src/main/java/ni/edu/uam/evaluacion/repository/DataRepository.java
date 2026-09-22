9e1800b5b746b55608d07e5dbadffe4920881a56
Steven Alexander flores Obregon <safloreso@uamv.edu.ni>
2026-09-21 18:50:06 -0600

    fix DataRepository

--- src/main/java/ni/edu/uam/evaluacion/repository/DataRepository.java
+++ src/main/java/ni/edu/uam/evaluacion/repository/DataRepository.java
@@ -1,8 +1,8 @@
-package evaluacion.repository;
+package ni.edu.uam.evaluacion.repository;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import sistema.model.Solicitud;
+import ni.edu.uam.evaluacion.modelo.Solicitud;
 
 public class DataRepository {
     // Lista reactiva global en memoria RAM
